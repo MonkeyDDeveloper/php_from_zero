c7d2ee016e192328d61846806d75d85dc90980f9
Javier Fray <108998691+MonkeyDDeveloper@users.noreply.github.com>
2024-12-19 18:26:22 -0500

    Add favicon

--- sections/header.php
+++ sections/header.php
@@ -1,5 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <title>My page with php</title>
+    <!-- add an icon here from the local path -->
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.classless.min.css">
 </head>
